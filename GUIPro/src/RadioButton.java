4d337c42fb023a3f1648788af0c44c55944ca6f0
Piotr Drelichowski <165167977+PiotrDrelichowksi@users.noreply.github.com>
2024-06-28 17:40:10 +0200

    Add files via upload

---
+++ GUIPro/src/RadioButton.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+public class RadioButton extends JRadioButton {
+    RadioButton(String text){
+        super(text);
+        setBackground(new Color(130,38,38));
+        setForeground(new Color(251,243,54));
+    }
+}
